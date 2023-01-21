b53aac6a36a2ffdfa246201fd89feae330e55e3e
ethicolorr <xex.opy@gmail.com>
2023-01-22 02:34:44 +0300

    load project

---
+++ const.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+x1 = 0
+y1 = 0
+x2 = 100
+y2 = 100
+
+lower_green = np.array([30, 128, 56])
+upper_green = np.array([90, 255, 255])
+lower_purple = np.array([125, 128, 56])
+upper_purple = np.array([175, 255, 255])
+lower_yellow = np.array([15, 128, 56])
+upper_yellow = np.array([30, 255, 255])
+
+shift = 20
---
+++ main.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+import const
+from numba import njit
+
+
+def keyboard(cap):
+    match cv2.waitKeyEx(1):
+        case 2424832:  # влево
+            if const.x1 != 0:
+                const.x1 = const.x1 - const.shift
+                const.x2 = const.x2 - const.shift
+        case 2490368:  # вверх
+            if const.y1 != 0:
+                const.y1 = const.y1 - const.shift
+                const.y2 = const.y2 - const.shift
+        case 2555904:  # вправо
+            if const.x2 <= cap.get(3):
+                const.x1 = const.x1 + const.shift
+                const.x2 = const.x2 + const.shift
+        case 2621440:  # вниз
+            if const.y2 <= cap.get(4):  # down
+                const.y1 = const.y1 + const.shift
+                const.y2 = const.y2 + const.shift
+
+
+def detectColor(hsv_frame):
+    mask_green = cv2.inRange(hsv_frame, const.lower_green, const.upper_green)
+    mask_purple = cv2.inRange(hsv_frame, const.lower_purple, const.upper_purple)
+    mask_yellow = cv2.inRange(hsv_frame, const.lower_yellow, const.upper_yellow)
+    sums = [np.sum(mask_green), np.sum(mask_purple), np.sum(mask_yellow)]
+    return matchColor(sums)
+
+
+@njit
+def matchColor(counts):
+    ix = np.argmax(counts) if len(counts) > 0 else -1
+    if counts[ix] > 0:
+        match ix:
+            case 0:
+                return 'green'
+            case 1:
+                return 'purple'
+            case 2:
+                return 'yellow'
+    return 'unknown'
+
+
+@njit
+def detectColorNp(hsv_frame):
+    height, width, channels = hsv_frame.shape
+    colors = []
+    for w in range(0, width):
+        for h in range(0, height):
+            px = hsv_frame[h, w]
+            if (const.lower_green < px).all() and (const.upper_green > px).all():
+                colors.append(0)
+            elif (const.lower_purple < px).all() and (const.upper_purple > px).all():
+                colors.append(1)
+            elif (const.lower_yellow < px).all() and (const.upper_yellow > px).all():
+                colors.append(2)
+    if len(colors) == 0:
+        return matchColor(np.array(colors))
+    else:
+        counts = np.bincount(np.array(colors))
+        return matchColor(counts)
+
+
+def main(file):
+    cap = cv2.VideoCapture(file)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if frame is None:
+            break
+        hsv_frame = cv2.cvtColor(frame[const.y1: const.y2, const.x1:const.x2], cv2.COLOR_BGR2HSV)
+        keyboard(cap)
+        cv2.rectangle(frame, (const.x1, const.y1), (const.x2, const.y2), (0, 0, 255), 2)
+        cv2.putText(frame, detectColorNp(hsv_frame), (const.x1 + 10, const.y1 + 80), 0, 0.6, (0, 0, 255), 2)
+        cv2.imshow('{}'.format(file[5:-4]), frame)
+        if cv2.waitKeyEx(1) == 27:
+            break
+
+
+main("data/1975.mp4")
+
